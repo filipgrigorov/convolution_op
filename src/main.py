e416c8220a602faa5ed0a84ff39fa44ce1ecc044
Filip Grigorov <filip_can@hotmail.com>
2021-09-22 18:53:10 +0300

    * Fixed the padding

--- src/main.py
+++ src/main.py
@@ -4,8 +4,9 @@ np.set_printoptions(precision=2, suppress=True)
 
 def Conv2d(tensor, nkernels, ksize, stride=1, padding=0):
     kernels = []
-    for idx in range(0, nkernels):
-        kernels.append(np.random.random((tensor.shape[-1], ksize, ksize)))
+    depth = tensor.shape[0]
+    for _ in range(0, nkernels):
+        kernels.append(np.random.random((depth, ksize, ksize)))
 
     feature_maps = []
     for kernel in kernels:
@@ -18,26 +19,27 @@ def convolve(tensor, kernel, stride=1, padding=0):
 
     print(f'tensor shape: {tensor.shape}\nkernel shape: {kernel.shape}\n')
         
-    padding *= 2
-    src = np.zeros(shape=(depth, arows + padding, acols + padding))
+    pad = 2 * padding
+    src = np.zeros(shape=(depth, arows + pad, acols + pad))
     _, rows, cols = src.shape
-    src[..., padding // 2 : arows + 1, padding // 2 : acols + 1] = tensor
+    print(src.shape)
+    src[:, padding : -padding, padding : -padding] = tensor
+    print(src)
 
     if __debug__:
         print(f'src: \n{src}')
    
     dst = np.zeros(shape=(
-        int((arows - krows + padding) / stride + 1), 
-        int((acols - kcols + padding) / stride + 1))
-    )
+        int(((arows - krows + padding) / stride)) + 1, 
+        int(((acols - kcols + padding) / stride)) + 1
+    ))
 
     if __debug__:
         print(f'dst size: {dst.shape}')
 
-    # TODO: Add stride support
     print(f'{rows}-{cols} :: {krows}-{kcols}')
-    for row in range(0, rows - krows + 1):
-        for col in range(0, cols - kcols + 1):
+    for row in range(0, rows - krows, stride):
+        for col in range(0, cols - kcols, stride):
             for d in range(0, depth):
                 for krow in range(0, krows):
                     kkrow = krow + row
@@ -56,8 +58,8 @@ if __name__ == '__main__':
 
     tensor = np.expand_dims(tensor, axis=0)
 
-    print(tensor)
+    print(f'tensor: \n{tensor}\n')
 
-    feature_map = Conv2d(tensor, nkernels=1, ksize=2, stride=1, padding=1)
+    feature_map = Conv2d(tensor, nkernels=1, ksize=2, stride=2, padding=1)
     print(feature_map)
 
